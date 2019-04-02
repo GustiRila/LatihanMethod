cac8d5838389d78c081867ce4ee5d5c43f92e669
GustiRila <praja.rila31@gmail.com>
2019-04-02 14:40:12 +0700

    LatihanMethod

---
+++ Latihan Method/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hasil;
+
+            Calculator calculator = new Calculator();
+
+            hasil = calculator.Penjumlah(10, 2);
+            CetakHasil("Hasil penjumlah: " + hasil);
+
+            hasil = calculator.Penjumlah(10, 2, 3);
+            CetakHasil("Hasil penjumlah overload: " + hasil);
+
+            hasil = calculator.Pengurangan(7, 2);
+            CetakHasil("Hasil pengurangan: " + hasil);
+
+            hasil = Calculator.Perkalian(5, 2);
+            CetakHasil("Hasil perkalian: " + hasil);
+
+            Console.ReadKey();
+        }
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
